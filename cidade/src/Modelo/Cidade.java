6056ec9fb01fea440fb0c6e0e533c6a1688ab9f7
Administrador <Administrador@10.10.4.9>
2019-06-25 16:49:26 -0300

    quarta versao

---
+++ cidade/src/Modelo/Cidade.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Cidade {
+    private int cod;
+    private String nome;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" + "cod=" + cod + ", nome=" + nome + '}';
+    }
+    
+    
+    
+}
